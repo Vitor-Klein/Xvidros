6553a6d18b7bcce9e62de71c3f0c76011429256e
Vitor Klein <vitorhklein2@gmail.com>
2020-03-30 23:41:02 -0300

    mobile

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop:Constants.statusBarHeight + 20,
+        backgroundColor: '#363636'   
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#4F4F4F',
+        marginBottom: 16,
+        marginTop: 48,
+    },
+
+    incidentProperty:{
+        fontSize: 14,
+        color: '#D3D3D3',
+        fontWeight: 'bold',
+        marginBottom: 24,
+    },
+
+    incidentValue:{
+       marginTop: 8,
+       fontSize: 15, 
+       color: '#C0C0C0',
+    },
+
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#4F4F4F',
+        marginBottom: 16,
+    },
+
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#D3D3D3',
+        lineHeight: 30,
+    },
+
+    heroDescription: {
+        fontSize: 15,
+        color: '#C0C0C0',
+        marginTop: 16,
+    },
+
+    actions: {
+        marginTop: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',      
+    },
+
+    action: {
+        backgroundColor: '#E02041',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    actionText: {
+        color: '#4F4F4F',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+})
